2b13e0b878318137d263ad5c47c2ec7560f77df5
Z User <z@container>
2026-05-05 09:55:20 +0000

    3010f4a5-066e-4e30-8ebd-be2ef6056a7e

---
+++ keep-alive.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+// Keep-alive wrapper for Next.js dev server
+const { spawn } = require('child_process');
+const fs = require('fs');
+const logFile = fs.openSync('/home/z/my-project/dev.log', 'a');
+
+const child = spawn('node', ['node_modules/.bin/next', 'dev', '-p', '3000'], {
+  cwd: '/home/z/my-project',
+  stdio: [logFile, logFile, logFile],
+  detached: true,
+  env: { ...process.env }
+});
+
+child.unref();
+
+// Keep this process alive
+setInterval(() => {
+  try {
+    process.kill(child.pid, 0);
+  } catch {
+    // Child died, this process can exit too
+    process.exit(1);
+  }
+}, 5000);
+
+process.on('SIGTERM', () => process.exit(0));
+process.on('SIGINT', () => process.exit(0));
